e6b02148f4a402ad38d4b26f1e9db76ccc59cd80
Joham <jklarenbeek@gmail.com>
2023-06-09 11:52:55 +0200

    refactored example 03 to use debug-print.h

--- examples/03-pt-finally/03-pt-finally.ino
+++ examples/03-pt-finally/03-pt-finally.ino
@@ -4,16 +4,17 @@
 
 #include <protoduino.h>
 #include <sys/pt.h>
+#include <sys/debug-print.h>
 
 static ptstate_t protothread1(struct pt *self)
 {
   PT_BEGIN(self);
 
-  Serial.println("PT_BEGIN() protothread1");
+  print_line("PT_BEGIN() protothread1");
 
   forever: while(1)
   {
-    Serial.println("PT_WAIT_ONE() protothread1");
+    print_line("PT_WAIT_ONE() protothread1");
 
     PT_WAIT_ONE(self);
 
@@ -26,68 +27,35 @@ static ptstate_t protothread1(struct pt *self)
       break;
   }
 
-  Serial.println("PT_ENDED protothread1");
+  print_line("PT_ENDED protothread1");
 
   PT_CATCHANY(self)
   {
-    Serial.print("PT_CATCHANY() protothread1:");
-    Serial.println(PT_ERROR_STATE);
+    print_error("PT_CATCHANY() protothread1", PT_ERROR_STATE);
     PT_RETHROW(self);
   }
 
   PT_FINALLY(self)
   
-  Serial.println("PT_FINALLY() protothread1");
+  print_line("PT_FINALLY() protothread1");
 
   PT_END(self);
 }
 
 void setup()
 {
-  Serial.begin(9600);
-  
-  // if analog input pin 0 is unconnected, random analog
-  // noise will cause the call to randomSeed() to generate
-  // different seed numbers each time the sketch runs.
-  // randomSeed() will then shuffle the random function.
-  randomSeed(analogRead(0));
-
-  Serial.println(("Done setup, waiting 2 sec."));
-  
-  delay(2000);
-}
-
-void print_state(const ptstate_t state, const char *msg)
-{
-    if (state == PT_WAITING)
-        Serial.print("PT_WAITING");
-    else if (state == PT_YIELDED)
-        Serial.print("PT_YIELDED");
-    else if (state == PT_ENDED)
-        Serial.print("PT_ENDED");
-    else if (state == PT_EXITED)
-        Serial.print("PT_EXITED");
-    else if (state == PT_FINALIZED)
-        Serial.print("PT_FINALIZED");
-    else 
-    {
-        Serial.print("PT_ERROR:");
-        Serial.print(state);
-    }
-    Serial.print(" ");
-    Serial.print(msg);
-    Serial.println();
+  print_setup();
 }
 
 void test1_run()
 {
-  Serial.println("void test1_run() START");
+  print_line("void test1_run() START");
 
   static struct pt pt1;
 
   static ptstate_t state;
 
-  /* Initialize the protothread state variables. */
+  // set the protothread control structure to the beginning of the protothread
   PT_INIT(&pt1);
   while(PT_ISRUNNING(state = protothread1(&pt1)))
   {
@@ -97,6 +65,7 @@ void test1_run()
   print_state(state, "void test1_run()");
   delay(1000);
 
+  // set the protothread control structure to the finally control block
   PT_FINAL(&pt1);
   while(PT_ISRUNNING(state = protothread1(&pt1)))
   {
@@ -110,7 +79,8 @@ void test1_run()
 
 void loop()
 {
-    test1_run();
+  test1_run();
+  print_count++;
 
-    delay(2000);
+  delay(2000);
 }
