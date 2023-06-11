4a9d51a54196b915c3c35719ee10893f16cad9b1
Joham <jklarenbeek@gmail.com>
2023-06-11 13:27:02 +0200

    restored previous 03-pt-finally.ino example

--- examples/03-pt-finally/03-pt-finally.ino
+++ examples/03-pt-finally/03-pt-finally.ino
@@ -1,41 +1,86 @@
+/***
+ * 
+*/
+
 #include <protoduino.h>
+#include <sys/pt.h>
 #include <sys/debug-print.h>
 
-static ptstate_t protothread(struct pt *self)
+static ptstate_t protothread1(struct pt *self)
 {
-  //static uint8_t v;
   PT_BEGIN(self);
 
-  PT_WAIT_ONE(self); // returns PT_WAITING
+  print_line("PT_BEGIN() protothread1");
 
-  PT_RAISE(self, (PT_ERROR + random(0, 63)));
+  forever: while(1)
+  {
+    print_line("PT_WAIT_ONE() protothread1");
 
-  print_line("UNREACHABLE protothread");
+    PT_WAIT_ONE(self);
+
+    uint8_t v = random(0, 6);
+    if (v == 2)
+      PT_EXIT(self);
+    else if (v == 3)
+      PT_RAISE(self, PT_ERROR + v);
+    else if (v == 4)
+      break;
+  }
+
+  print_line("PT_ENDED protothread1");
 
   PT_CATCHANY(self)
   {
-    print_error("PT_CATCHANY() protothread", PT_ERROR_STATE);
+    print_error("PT_CATCHANY() protothread1", PT_ERROR_STATE);
+    PT_RETHROW(self);
   }
-  PT_END(self); // returns PT_FINALIZED
-}
 
-static struct pt pt1;
+  PT_FINALLY(self)
+  
+  print_line("PT_FINALLY() protothread1");
+
+  PT_END(self);
+}
 
 void setup()
 {
   print_setup();
+}
+
+void test1_run()
+{
+  print_line("void test1_run() START");
+
+  static struct pt pt1;
+
+  static ptstate_t state;
+
+  // set the protothread control structure to the beginning of the protothread
   PT_INIT(&pt1);
+  while(PT_ISRUNNING(state = protothread1(&pt1)))
+  {
+    print_state(state, "void test1_run()");
+    delay(1000);
+  }
+  print_state(state, "void test1_run()");
+  delay(1000);
+
+  // set the protothread control structure to the finally control block
+  PT_FINAL(&pt1);
+  while(PT_ISRUNNING(state = protothread1(&pt1)))
+  {
+    print_state(state, "void test1_run()");
+    delay(1000);  
+  }
+  print_state(state, "void test1_run()");
+  delay(1000);
+
 }
 
 void loop()
 {
+  test1_run();
   print_count++;
-  delay(1000);
 
-  ptstate_t state = protothread(&pt1);
-  print_state(state, pt1.lc, "< protothread");
-  if (state == PT_FINALIZED || PT_ISRUNNING(state))
-    return;
-  
-  PT_FINAL(&pt1);
+  delay(2000);
 }
